cdedaa5c59ccf54a1efcda697ac587aba8b37f67
DavidToroHrr <davidesteban0736@gmail.com>
2026-05-09 16:06:51 -0500

    project migrated to spring boot

---
+++ src/test/java/com/oficiar/backend/OficiarBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficiar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficiarBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
